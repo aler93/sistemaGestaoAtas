35a3a656d9e0525bcbfa88d0d442184ca75db2db
Alisson Naimayer <anaimayer3@gmail.com>
2020-10-08 19:07:47 -0300

    hostos

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'usuario.Usuario'
 
